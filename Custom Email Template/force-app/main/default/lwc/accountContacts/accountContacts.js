e55ab62bb064e39356067455caa889eea7799be6
keshari-rakesh <rakesh.keshari@aimdek.com>
2023-09-19 12:15:42 +0530

    Renamed the variables

--- Custom Email Template/force-app/main/default/lwc/accountContacts/accountContacts.js
+++ Custom Email Template/force-app/main/default/lwc/accountContacts/accountContacts.js
@@ -28,9 +28,9 @@ export default class AccountContacts extends LightningElement {
     }
 
     async getContacts(){
-        const data = await getAccountContacts({accountId:this.accountId});
-        this.contacts = data;
-        //System.debug('Data:',data);
+        const changeddata = await getAccountContacts({accountId:this.accountId});
+        this.contacts = changeddata;
+        System.debug('Data:',changeddata);
     }
 
     // Encapsulate logic for Lightning message service subscribe and unsubsubscribe
